9dfac287547676040d4bc1830f70f9831b63b634
João Lenon <lenonSec7@gmail.com>
2022-03-15 14:57:54 -0300

    feat: refactor Model to become a static class

---
+++ src/Types/OmitModelMethods.ts
@@ -0,0 +1,16 @@
+
+/**
+ * @secjs/orm
+ *
+ * (c) João Lenon <lenon@secjs.com.br>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+ export class OmitModelMethods {
+
+
+    public constructor() {}
+ }
+
