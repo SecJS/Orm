1668acb20ead8d74fec39743818c282dffc1cb92
João Lenon <lenonSec7@gmail.com>
2022-03-17 16:02:23 -0300

    test: create test data handler to start the test suite

---
+++ tests/Utils/TestDataHandler.ts
@@ -0,0 +1,113 @@
+/**
+ * @secjs/orm
+ *
+ * (c) João Lenon <lenon@secjs.com.br>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { Knex } from 'knex'
+import { DatabaseContract } from '@secjs/database'
+
+export class TestDataHandler {
+  static DB: DatabaseContract
+
+  static setDB(DB: DatabaseContract): typeof TestDataHandler {
+    TestDataHandler.DB = DB
+
+    return this
+  }
+
+  static async dropTables() {
+    await TestDataHandler.DB.dropTable('product_details')
+    await TestDataHandler.DB.dropTable('users_roles')
+    await TestDataHandler.DB.dropTable('products')
+    await TestDataHandler.DB.dropTable('roles')
+    await TestDataHandler.DB.dropTable('users')
+  }
+
+  static async createTables() {
+    await TestDataHandler.DB.createTable('users', (tableBuilder: Knex.TableBuilder) => {
+      tableBuilder.increments('id').primary()
+      tableBuilder.string('name').nullable()
+      tableBuilder.string('email').nullable()
+      tableBuilder.timestamps(true, true, true)
+    })
+
+    await TestDataHandler.DB.createTable('roles', (tableBuilder: Knex.TableBuilder) => {
+      tableBuilder.increments('id').primary()
+      tableBuilder.string('name').nullable()
+      tableBuilder.string('description').nullable()
+      tableBuilder.timestamps(true, true, true)
+    })
+
+    await TestDataHandler.DB.createTable('users_roles', (tableBuilder: Knex.TableBuilder) => {
+      tableBuilder.increments('id').primary()
+      tableBuilder.integer('users_id').unsigned().references('id').inTable('users')
+      tableBuilder.integer('roles_id').unsigned().references('id').inTable('roles')
+    })
+
+    await TestDataHandler.DB.createTable('products', (tableBuilder: Knex.TableBuilder) => {
+      tableBuilder.increments('id').primary()
+      tableBuilder.string('name').nullable()
+      tableBuilder.integer('quantity').nullable().defaultTo(0)
+      tableBuilder.timestamps(true, true, true)
+      tableBuilder.integer('userId').references('id').inTable('users')
+    })
+
+    await TestDataHandler.DB.createTable('product_details', (tableBuilder: Knex.TableBuilder) => {
+      tableBuilder.increments('id').primary()
+      tableBuilder.string('detail').nullable()
+      tableBuilder.timestamps(true, true, true)
+      tableBuilder.integer('productId').references('id').inTable('products')
+    })
+  }
+
+  static async createData() {
+    const [userId] = await TestDataHandler.DB.buildTable('users').insert({ name: 'Victor', email: 'txsoura@gmail.com' })
+
+    const [roleId1, roleId2] = await TestDataHandler.DB.buildTable('roles').insert([
+      {
+        name: 'Admin',
+        description: 'Server Admin',
+      },
+      { name: 'Owner', description: 'Server Owner' },
+    ])
+
+    await TestDataHandler.DB.buildTable('users_roles').insert([
+      { users_id: userId, roles_id: roleId1 },
+      {
+        users_id: userId,
+        roles_id: roleId2,
+      },
+    ])
+
+    const ids = await TestDataHandler.DB.buildTable('products').insert([
+      {
+        name: 'iPhone 10',
+        quantity: 10,
+        userId: userId,
+      },
+      {
+        name: 'iPhone 10',
+        quantity: 10,
+        userId: userId,
+      },
+      {
+        name: 'iPhone 10',
+        quantity: 10,
+        userId: userId,
+      },
+    ])
+
+    const promises = ids.map(id =>
+      TestDataHandler.DB.buildTable('product_details').insert([
+        { detail: '128 GB', productId: id },
+        { detail: 'Black', productId: id },
+      ]),
+    )
+
+    await Promise.all(promises)
+  }
+}
--- tests/model.spec.ts
+++ tests/model.spec.ts
@@ -8,9 +8,9 @@
  */
 
 import 'reflect-metadata'
-import { Knex } from 'knex'
 import { Product } from './Stubs/Models/Product'
 import { UserModel } from './Stubs/Models/UserModel'
+import { TestDataHandler } from './Utils/TestDataHandler'
 import { Database, DatabaseContract } from '@secjs/database'
 
 describe('\n Model Class', () => {
@@ -23,84 +23,10 @@ describe('\n Model Class', () => {
   })
 
   beforeEach(async () => {
-    await DB.createTable('users', (tableBuilder: Knex.TableBuilder) => {
-      tableBuilder.increments('id').primary()
-      tableBuilder.string('name').nullable()
-      tableBuilder.string('email').nullable()
-      tableBuilder.timestamps(true, true, true)
-    })
+    TestDataHandler.setDB(DB)
 
-    await DB.createTable('roles', (tableBuilder: Knex.TableBuilder) => {
-      tableBuilder.increments('id').primary()
-      tableBuilder.string('name').nullable()
-      tableBuilder.string('description').nullable()
-      tableBuilder.timestamps(true, true, true)
-    })
-
-    await DB.createTable('users_roles', (tableBuilder: Knex.TableBuilder) => {
-      tableBuilder.increments('id').primary()
-      tableBuilder.integer('users_id').unsigned().references('id').inTable('users')
-      tableBuilder.integer('roles_id').unsigned().references('id').inTable('roles')
-    })
-
-    await DB.createTable('products', (tableBuilder: Knex.TableBuilder) => {
-      tableBuilder.increments('id').primary()
-      tableBuilder.string('name').nullable()
-      tableBuilder.integer('quantity').nullable().defaultTo(0)
-      tableBuilder.timestamps(true, true, true)
-      tableBuilder.integer('userId').references('id').inTable('users')
-    })
-
-    await DB.createTable('product_details', (tableBuilder: Knex.TableBuilder) => {
-      tableBuilder.increments('id').primary()
-      tableBuilder.string('detail').nullable()
-      tableBuilder.timestamps(true, true, true)
-      tableBuilder.integer('productId').references('id').inTable('products')
-    })
-
-    const [userId] = await DB.buildTable('users').insert({ name: 'Victor', email: 'txsoura@gmail.com' })
-    const [roleId1, roleId2] = await DB.buildTable('roles').insert([
-      {
-        name: 'Admin',
-        description: 'Server Admin',
-      },
-      { name: 'Owner', description: 'Server Owner' },
-    ])
-
-    await DB.buildTable('users_roles').insert([
-      { users_id: userId, roles_id: roleId1 },
-      {
-        users_id: userId,
-        roles_id: roleId2,
-      },
-    ])
-
-    const ids = await DB.buildTable('products').insert([
-      {
-        name: 'iPhone 10',
-        quantity: 10,
-        userId: userId,
-      },
-      {
-        name: 'iPhone 10',
-        quantity: 10,
-        userId: userId,
-      },
-      {
-        name: 'iPhone 10',
-        quantity: 10,
-        userId: userId,
-      },
-    ])
-
-    const promises = ids.map(id =>
-      DB.buildTable('product_details').insert([
-        { detail: '128 GB', productId: id },
-        { detail: 'Black', productId: id },
-      ]),
-    )
-
-    await Promise.all(promises)
+    await TestDataHandler.createTables()
+    await TestDataHandler.createData()
   })
 
   it('should be able to create a new product', async () => {
@@ -144,17 +70,17 @@ describe('\n Model Class', () => {
   it('should return all data from Product model with user and productDetails included', async () => {
     const models = await Product.includes('user').includes('productDetails').findMany()
 
-    expect(models[0].id).toBe(1)
-    expect(models[0].userModelId).toBe(1)
-    expect(models[0].name).toBe('iPhone 10')
-    expect(models[0].user.idPrimary).toBe(1)
-    expect(models[0].user.name).toBe('Victor')
-    expect(models[0].productDetails[0].id).toBe(1)
-    expect(models[0].productDetails[0].detail).toBe('128 GB')
-    expect(models[0].productDetails[0].productModelId).toBe(1)
-    expect(models[0].productDetails[1].id).toBe(2)
-    expect(models[0].productDetails[1].detail).toBe('Black')
-    expect(models[0].productDetails[1].productModelId).toBe(1)
+    const firstModelJson = models[0].toJSON()
+
+    expect(firstModelJson.id).toBe(1)
+    expect(firstModelJson.userModelId).toBe(1)
+    expect(firstModelJson.name).toBe('iPhone 10')
+    expect(firstModelJson.user.idPrimary).toBe(1)
+    expect(firstModelJson.user.name).toBe('Victor')
+    expect(firstModelJson.productDetails[0].detail).toBe('128 GB')
+    expect(firstModelJson.productDetails[0].productModelId).toBe(1)
+    expect(firstModelJson.productDetails[1].detail).toBe('Black')
+    expect(firstModelJson.productDetails[1].productModelId).toBe(1)
   })
 
   it('should return one data from Product model with user and productDetails included', async () => {
@@ -188,11 +114,7 @@ describe('\n Model Class', () => {
   })
 
   afterEach(async () => {
-    await DB.dropTable('product_details')
-    await DB.dropTable('users_roles')
-    await DB.dropTable('products')
-    await DB.dropTable('roles')
-    await DB.dropTable('users')
+    await TestDataHandler.dropTables()
   })
 
   afterAll(async () => {
