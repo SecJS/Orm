519d9bee88a9c4340e21a6f5c4ef8bafb80e9500
João Lenon <lenonSec7@gmail.com>
2022-03-17 16:07:41 -0300

    style: export contracts in ORM index

--- index.ts
+++ index.ts
@@ -10,15 +10,23 @@
 import 'reflect-metadata'
 
 // Decorators
-export * from './src/Decorators/HasMany'
 export * from './src/Decorators/Column'
 export * from './src/Decorators/HasOne'
+export * from './src/Decorators/HasMany'
 export * from './src/Decorators/BelongsTo'
 export * from './src/Decorators/ManyToMany'
 
 // Contracts
 export * from './src/Contracts/ColumnOptions'
 export * from './src/Contracts/ColumnContract'
+export * from './src/Contracts/HasOneOptions'
+export * from './src/Contracts/HasOneContract'
+export * from './src/Contracts/HasManyOptions'
+export * from './src/Contracts/HasManyContract'
+export * from './src/Contracts/BelongsToOptions'
+export * from './src/Contracts/BelongsToContract'
+export * from './src/Contracts/ManyToManyOptions'
+export * from './src/Contracts/ManyToManyContract'
 
 // Model
 export * from './src/Model'
